7c7fb26c4fea3979eae1cee4edd002f40db02555
zeynepgenc6 <zeynepgenc6@gmail.com>
2024-04-20 01:54:18 -0500

    changed file js, css and html

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,28 @@
+function updateTime(){
+    //london
 let londonElement = document.querySelector("#london");
 let londonDateElement = londonElement.querySelector(".date");
 let londonTimeElement = londonElement.querySelector(".time");
 let londonTime = moment().tz("Europe/London");
-londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
-londonTimeElement.innerHTML = `${londonTime.format("h:mm:ss")} <small>${londonTime.format("A")}</small>`;
-
+londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+//moscow
 let moscowElement = document.querySelector("#moscow");
 let moscowDateElement = moscowElement.querySelector(".date");
 let moscowTimeElement = moscowElement.querySelector(".time");
 let moscowTime = moment().tz("Europe/Moscow");
-moscowDateElement.innerHTML = moment().format("MMMM Do YYYY");
-moscowTimeElement.innerHTML = `${moscowTime.format(
-  "h:mm:ss"
-)} <small>${moscowTime.format("A")}</small>`;
+moscowDateElement.innerHTML = moscowTime.format("MMMM Do YYYY");
+moscowTimeElement.innerHTML = moscowTime.format("h:mm:ss [<small>]A[</small>]");
+//alaska
+let alaskaElement = document.querySelector("#alaska");
+let alaskaDateElement = alaskaElement.querySelector(".date");
+let alaskaTimeElement = alaskaElement.querySelector(".time");
+let alaskaTime = moment().tz("Alaska");
+alaskaDateElement.innerHTML = alaskaTime.format("MMMM Do YYYY");
+alaskaTimeElement.innerHTML = alaskaTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
+
+
+
