c69c4a89353289cce4efe1b8548e63e30b20a60f
zeynepgenc6 <zeynepgenc6@gmail.com>
2024-04-21 16:16:16 -0500

    changed element name in js

--- javascript/index.js
+++ javascript/index.js
@@ -32,12 +32,12 @@ function updateTime() {
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
    <div class="city" >
-        <div><h2>${cityTimeZone}</h2>
+        <div><h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div></div>
         <div class="time">${cityTime.format(
           "h:mm:ss"
