9b47119aac4755fab379bcf97788a75682d01fb7
zeynepgenc6 <zeynepgenc6@gmail.com>
2024-04-21 16:10:02 -0500

    updated file js

--- javascript/index.js
+++ javascript/index.js
@@ -1,31 +1,34 @@
 function updateTime() {
   //london
   let londonElement = document.querySelector("#london");
+  if(londonElement){
   let londonDateElement = londonElement.querySelector(".date");
   let londonTimeElement = londonElement.querySelector(".time");
   let londonTime = moment().tz("Europe/London");
   londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
   londonTimeElement.innerHTML = londonTime.format(
     "h:mm:ss [<small>]A[</small>]"
-  );
+  );}
   //moscow
   let moscowElement = document.querySelector("#moscow");
+  if (moscowElement){
   let moscowDateElement = moscowElement.querySelector(".date");
   let moscowTimeElement = moscowElement.querySelector(".time");
   let moscowTime = moment().tz("Europe/Moscow");
   moscowDateElement.innerHTML = moscowTime.format("MMMM Do YYYY");
   moscowTimeElement.innerHTML = moscowTime.format(
     "h:mm:ss [<small>]A[</small>]"
-  );
+  );}
   //alaska
   let alaskaElement = document.querySelector("#alaska");
+  if (alaskaElement){
   let alaskaDateElement = alaskaElement.querySelector(".date");
   let alaskaTimeElement = alaskaElement.querySelector(".time");
   let alaskaTime = moment().tz("America/Anchorage");
   alaskaDateElement.innerHTML = alaskaTime.format("MMMM Do YYYY");
   alaskaTimeElement.innerHTML = alaskaTime.format(
     "h:mm:ss [<small>]A[</small>]"
-  );
+  );}
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
